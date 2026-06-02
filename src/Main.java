e6a5fbfa9c7301ae3b770b7923527cf6b4f09ea9
ndri eunice <eunicenini3@gmail.com>
2026-06-02 17:21:33 +0000

    Ajout du code de validité

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,22 @@
 public class Main {
+    public static void main(String[] args) {
+        Reine reine = new Reine();
 
+        // Position actuelle de la reine
+        reine.position = new Position();
+        reine.position.row = 4;
+        reine.position.column = 4;
+
+        // Nouvelle position à tester
+        Position newPos = new Position();
+        newPos.row = 6;
+        newPos.column = 6;
+
+        // Affichage du résultat
+        System.out.println(reine.isValidMove(newPos));
+    }
 }
+
 class Piece {
     Position position;
     boolean isValidMove(Position newPosition){
@@ -14,6 +30,20 @@ class Piece {
 
 class Reine extends Piece {
     @Override
+    boolean isValidMove(Position newPosition) {
+
+        //  vérifier si c'est dans le plateau
+        if (!super.isValidMove(newPosition)) {
+            return false;
+        }
+
+        // déplacement diagonal, éplacement vertical et déplacement horizontal
+        if ((Math.abs(newPosition.column - this.position.column) == Math.abs(newPosition.row - this.position.row)) || (newPosition.column == this.position.column) || (newPosition.row == this.position.row)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
 
 class Position{
